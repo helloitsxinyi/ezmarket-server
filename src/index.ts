4fc95607b5c96a0c76764bd71dd36e6fcab05251
Xinyi <xinzwerkz@gmail.com>
2021-12-19 13:03:05 +0800

    add user model and update items model

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { EzBackend, EzModel, Type } from "@ezbackend/common";
 import { EzOpenAPI } from "@ezbackend/openapi";
 import { EzDbUI } from "@ezbackend/db-ui";
 import { EzCors } from "@ezbackend/cors";
+import { EzUser } from "@ezbackend/auth";
 
 const app = new EzBackend();
 
@@ -13,13 +14,35 @@ app.addApp(new EzCors());
 //---Plugins---
 
 //Models are also ezapps in ezbackend
-const model = new EzModel("Items", {
+const Items = new EzModel("items", {
   shopName: Type.VARCHAR,
   itemName: Type.VARCHAR,
   price: Type.DOUBLE,
   available: Type.BOOL,
+  user: {
+    type: Type.MANY_TO_ONE,
+    target: "user",
+    inverseSide: "item",
+    joinColumn: true,
+    onDelete: "SET NULL",
+    nullable: true,
+  },
+  userId: {
+    type: Type.INT,
+    nullable: true,
+  },
 });
 
-app.addApp(model, { prefix: "/items" });
+export const User = new EzUser("user", ["google"], {
+  item: {
+    type: Type.ONE_TO_MANY,
+    target: "items",
+    inverseSide: "user",
+    nullable: true,
+  },
+});
+
+app.addApp(Items, { prefix: "/items" });
+app.addApp(User, { prefix: "/users" });
 
 app.start({ address: "0.0.0.0", server: { trustProxy: true } });
