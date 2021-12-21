fac2745a3b67ec4f8b6fe04d141e2db90888c2d6
Xinyi <xinzwerkz@gmail.com>
2021-12-21 11:48:34 +0800

    attach database and update app.start opts

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { EzBackend, EzModel, Type } from "@ezbackend/common";
 import { EzOpenAPI } from "@ezbackend/openapi";
 import { EzDbUI } from "@ezbackend/db-ui";
 import { EzCors } from "@ezbackend/cors";
+import type { createConnection } from "typeorm";
 // import { EzUser } from "@ezbackend/auth";
 
 const app = new EzBackend();
@@ -21,6 +22,16 @@ const Items = new EzModel("items", {
   available: Type.BOOL,
 });
 
+const ormConfig: Parameters<typeof createConnection>[0] = {
+  type: "postgres",
+  url: process.env.DATABASE_URL,
+  synchronize: true,
+  ssl: process.env.NODE_ENV === "production",
+};
+
 app.addApp(Items, { prefix: "/items" });
 
-app.start({ address: "0.0.0.0", server: { trustProxy: true } });
+app.start({
+  backend: { typeorm: ormConfig, listen: { address: "0.0.0.0" } },
+  server: { trustProxy: true },
+});
